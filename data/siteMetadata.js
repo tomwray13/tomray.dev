ee1de783706c3d5e979d6d833231bd3889b4847e
Tom Ray <tomwray13@gmail.com>
2022-04-29 09:44:23 +0100

    Tweaking footer

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,21 +1,17 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'Tom Ray - Learn Server Side GraphQL',
+  author: 'Tom Ray',
   headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'A blog focused on the intersection of GCP and Javascript',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://tomray.dev',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  github: 'https://github.com/tomwray13',
+  twitter: 'https://twitter.com/bytomray',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
