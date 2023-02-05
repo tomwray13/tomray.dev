bcf692201ae1448a8a951a7191809598ca28409b
Tom Ray <tomwray13@gmail.com>
2023-02-05 19:06:07 +0000

    Replace GTM with Fathom

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com giscus.app;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.usefathom.com giscus.app;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,42 +1,44 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
-
 import '@fontsource/inter/variable-full.css'
-
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 import { useEffect } from 'react'
-import TagManager from 'react-gtm-module'
+import * as Fathom from 'fathom-client'
+import { useRouter } from 'next/router'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
   useEffect(() => {
-    TagManager.initialize({
-      gtmId: process.env.GTM_ID,
-      auth: process.env.GTM_AUTH,
-      preview: process.env.GTM_PREVIEW,
+    Fathom.load(process.env.FATHOM, {
+      includedDomains: ['tomray.dev'],
     })
-  }, [])
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  })
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <script
-          defer
-          type="text/javascript"
-          src="https://api.pirsch.io/pirsch.js"
-          id="pirschjs"
-          data-code={process.env.PIRSCH_ID}
-        ></script>
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
